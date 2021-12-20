7e06b26ef57f0a05e2552257020ff7095e71d3f7
JunHyeokChoi93 <cjun025@gmail.com>
2021-12-20 22:53:50 +0900

    1617 진행

--- 1617/B.py
+++ 1617/B.py
@@ -1,13 +1,14 @@
-def divisor_find(num):
-    for i in range(6,num):
-        if num % i == 0:
+import math
+def find_div(num):
+    for i in range(num//2,1,-1):
+        if math.gcd(i,num-i) == 1:
             return i
-        else:
-            continue
-    return None
-
 problem_num = int(input())
 for i in range(problem_num):
     S = int(input())
-    c = divisor_find(S)
+    c = 1
+    a = find_div(S-1)
+    b = S - 1 - a
+    print(a,b,c)
+
 
