564a58a0cc789760be27856f34825d99035d8418
JunHyeokChoi93 <cjun025@gmail.com>
2021-12-21 17:42:14 +0900

    1617 진행

--- 1617/C.py
+++ 1617/C.py
@@ -1,27 +1,26 @@
 problem_num = int(input())
 for i in range(problem_num):
     n = int(input())
-    arr = list(map(int,input().split()))
-    target = set(range(1,n+1))
+    arr = list(map(int, input().split()))
+    target = set(range(1, n + 1))
     target_arr = set()
     target_dup = []
-    for a in arr:
-        if a in target and a not in target_arr:
-            target_arr.add(a)
+    for i in range(n):
+        if arr[i] in target and arr[i] not in target_arr:
+            target_arr.add(arr[i])
         else:
-            target_dup.append(a)
-    target_dup.sort()
+            target_dup.append(arr[i])
     dum = list(target - target_arr)
-    flag = False
-    for i in range(len(dum)):
-        if dum[i] > (target_dup[i] + 1) // 2 - 1:
-            prn = -1
-            flag = True
+    pr = 0
+    dum.sort()
+    target_dup.sort()
+    for i in range(len(target_dup)):
+        if (target_dup[i] - 1) // 2 < dum[i]:
+            pr = -1
             break
-    if flag:
-        print(prn)
+    if pr:
+        print(-1)
     else:
         print(len(target_dup))
-
 
 
