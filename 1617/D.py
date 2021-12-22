544046ce06127c9a403fc1539c65b50e618fb78a
JunHyeokChoi93 <cjun025@gmail.com>
2021-12-22 20:12:14 +0900

    1617 진행

--- 1617/D.py
+++ 1617/D.py
@@ -2,7 +2,6 @@ import sys
 problem_num = int(input())
 for problem in range(problem_num):
     n = int(input())
-    prev = None
     arr = [-1]*n
     judge_arr = [-1] * n
     for i in range(n):
@@ -15,13 +14,55 @@ for problem in range(problem_num):
         print('?',a,b,c)
         cur = int(input())
         judge_arr[i] = cur
-        if prev != None:
-            if prev != cur:
-                break
-            prev = cur
-    if prev ==
+    for i in range(n):
+        diff = judge_arr[i-1] - judge_arr[i]
+        if diff == 1:
+            arr[i - 2] = 1
+            arr[i - 1] = 1
+            arr[(i + 2)%(n+1)] = 0
+        elif diff == -1:
+            arr[i - 2] = 0
+            arr[i - 1] = 0
+            arr[(i + 2)%n] = 1
+    for i in range(n):
+        if arr[i] == -1:
+            j = i -1
+            if arr[j] == -1:
+                continue
+            if j == 0:
+                judge_sub = [judge_arr[-1], judge_arr[0], judge_arr[1]]
+            elif j == - 1:
+                judge_sub = [judge_arr[-2], judge_arr[-1], judge_arr[0]]
+            else:
+                judge_sub = judge_arr[j - 1:j + 2]
+            i1,i2,i3,i4 = j-2,j-1,(j+1)%n,(j+2)%n
+            if arr[j] == 0:
+                if judge_sub == [1,1,1]:
+                    arr[i1] = 1;arr[i2] = 1;arr[i3] = 1;arr[i4] = 1
+                elif judge_sub == [0,1,0]:
+                    arr[i1] = 0;arr[i2] = 1;arr[i3] = 1;arr[i4] = 0
+                elif judge_sub == [1,1,0]:
+                    arr[i1] = 1;arr[j-1] = 1;arr[i3] = 1;arr[i4] = 0
+                elif judge_sub == [0,1,1]:
+                    arr[i1] = 1;arr[i2] = 0;arr[i3] = 0;arr[i4] = 0
+                elif judge_sub == [0,0,1]:
+                    arr[i2] = 0;arr[i3] = 1;arr[i4] = 1
+                elif judge_sub == [1,0,0]:
+                    arr[i1] = 1;arr[i2] = 1;arr[i3] = 0
 
-
+            elif arr[j] == 1:
+                if judge_sub == [0,0,0]:
+                    arr[i1] = 0;arr[i2] = 0;arr[i3] = 0;arr[i4] = 0
+                elif judge_sub == [1,0,1]:
+                    arr[i1] = 1;arr[i2] = 0;arr[i3] = 0;arr[i4] = 1
+                elif judge_sub == [0,0,1]:
+                    arr[i1] = 0;arr[i2] = 0;arr[i3] = 0;arr[i4] = 1
+                elif judge_sub == [1,0,0]:
+                    arr[i1] = 0;arr[i2] = 1;arr[i3] = 1;arr[i4] = 1
+                elif judge_sub == [1,1,0]:
+                    arr[i2] = 1;arr[i3] = 0;arr[i4] = 0
+                elif judge_sub == [0,1,1]:
+                    arr[i1] = 0;arr[i2] = 0;arr[i3] = 1
 
     print_line = []
     for i in range(n):
