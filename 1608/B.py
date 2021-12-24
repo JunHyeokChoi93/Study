87c7ab1ec4788723d7cd3417d1b454f1947f1c10
JunHyeokChoi93 <cjun025@gmail.com>
2021-12-24 18:14:24 +0900

    1617 진행

--- 1608/B.py
+++ 1608/B.py
@@ -1,11 +1,30 @@
 problem_num = int(input())
 for i in range(problem_num):
     arr = list(map(int,input().split()))
-    if abs(arr[1]-arr[2])>2 or arr[1] + arr[2] > arr[0] - 2:
+    if abs(arr[1]-arr[2])>=2 or arr[1] + arr[2] > arr[0] - 2:
         print(-1)
         continue
-    init = 10 ** 6
+
+    init = 0
+    lst = [init]
     if arr[1]>arr[2]:
-
-    elif arr[1]<arr[2]
-
+        for j in range(arr[1]):
+            lst.append(init + j + 1)
+            lst.append(init - j - 1)
+        for j in range(arr[0]-arr[1]-arr[2]-2):
+            lst.append(init-j-arr[1]-1)
+    if arr[1]<arr[2]:
+        for j in range(arr[2]):
+            lst.append(init - j - 1)
+            lst.append(init + j + 1)
+        for j in range(arr[0]-arr[1]-arr[2]-2):
+            lst.append(init+j+arr[2]+1)
+    if arr[1]==arr[2]:
+        for j in range(arr[2]):
+            lst.append(init + j + 1)
+            lst.append(init - j - 1)
+        for j in range(arr[0]-arr[1]-arr[2]-1):
+            lst.append(init+j+arr[2]+1)
+    min_val = min(lst)
+    lst = [str(i-min_val+1) for i in lst]
+    print(' '.join(lst))
