db0be329b5f0c80b0be75932e559d2b76820743d
cjun0 <cjun025@gmail.com>
2021-12-28 23:38:56 +0900

    1615 C problem

--- 1615/C.py
+++ 1615/C.py
@@ -6,6 +6,10 @@ for i in range(problem_num):
     if arr2.count('1') not in [arr1.count('1'),n-arr1.count('1')+1]:
         print(-1)
         continue
+    # same_0 = 0
+    # same_1 = 0
+    # diff_0 = 0
+    # diff_1 = 0
     same = 0
     diff = 0
     for j in range(len(arr1)):
@@ -13,4 +17,9 @@ for i in range(problem_num):
             same += 1
         else:
             diff += 1
-    print(min([same,diff]))
+    if arr1.count('1') == n-arr1.count('1')+1:
+        print(min([same,diff]))
+    elif arr2.count('1') == arr1.count('1'):
+        print(diff)
+    else:
+        print(same)
